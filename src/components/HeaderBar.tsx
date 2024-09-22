2d8d4c8830e7c29fc99676a793c86316843f7153
arthurpelitoo <arthur.pelito@escola.pr.gov.br>
2024-09-22 01:52:01 -0300

    estou apanhando pro tailwind, adicionei uma barrinha provisoria e tentando adicionar um botao simples e visual

---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,15 @@
+
+function HeaderBar(){
+
+    return (
+        <>
+        <header id="HeaderBar" className="bg-primary w-full h-16">
+            <div>
+                <button id="btnHeader" className="font-playfair rounded-extra-large text-primaryText text-2xl w-20 ">Sobre</button>
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default HeaderBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        pacifico: ['Pacifico', 'cursive'],
+        playfair: ['Playfair Display', 'serif'],
+      },
+      colors:{
+        primary: '#FBE7A1',  //amarelo claro
+        secondary: '#E0CF90', //amarelo um pouco escuro
+        highlight: '#FF6F61' //rosa leve
+      },
+      textColor:{
+        primaryText: '#402B18' //marrom escuro
+      },
+      borderRadius:{
+        'extra-large': '2rem' //borda bem arredondada
+      },
+      animation:{
+        'sumi-e-apareci': 'bg-secondary 0.5s ease'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui')
+  ],
 }
 
